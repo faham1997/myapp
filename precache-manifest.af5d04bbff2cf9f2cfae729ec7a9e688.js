48602a83a27c3b2d73eff4465acd36bf23949f39
faham1997 <nafidfaham08@gmail.com>
2020-03-26 22:45:20 +0600

    Updates

---
+++ precache-manifest.af5d04bbff2cf9f2cfae729ec7a9e688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b136a2a3e2dff7835e7ad1e2b73976e1",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "e24598080be0ed99eca0",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/myapp/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24598080be0ed99eca0",
+    "url": "/myapp/static/js/main.0f55d7b8.chunk.js"
+  },
+  {
+    "revision": "9dbb667345c34cd4c841",
+    "url": "/myapp/static/js/runtime-main.237f4126.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
